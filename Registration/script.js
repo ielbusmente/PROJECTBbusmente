5451103ca5d09225bd841316fcdba007658e752f
Edward Daniel J. Busmente <2018112067@ust-ics.mygbiz.com>
2020-02-24 05:28:45 +0800

    google tested only

--- Registration/script.js
+++ Registration/script.js
@@ -1,39 +1,46 @@
 var red = "rgb(255, 130, 130)";
 var green = "rgb(142, 255, 151)";
 var user = document.getElementById('login');
-var pass = document.getElementById('pass');
-var confPass = document.getElementById('confPass'); 
-var status = document.getElementById('stat');
 
 
-function submitValidation() {  
-    if (pass.value == "" ) {
-        pass.style.background = red;
-        return false;
-    }
-    if (confPass.value == "" ) {
-        confPass.style.background = red;
-        return false;
-    } 
+function submitValidation() {    
+    var pass = document.getElementById('pass');
+    var confPass = document.getElementById('confPass');
+    var str = document.getElementById('stat');  
     if (pass.value != confPass.value) {
         pass.style.background = red;
         confPass.style.background = red;  
-        status.innerHTML="Passwords don't match."
-        status.style.color = red;
+        str.innerHTML="Passwords don't match."
+        str.style.color = red;
         return false;
-    }   
-    alert(`Hello ${user.value.trim()}! You are now registered. Please check your email to confirm`+
-        ` your account.`);
+    }    
+    alert(`Hello ${user.value.trim()}! You are now registered. Please check your email to co`
+    + `nfirm your account.`);
     
 } 
-var passStrength = function() {
-    var cou = 0;
-    if (pass.value.includes(/a-z/g)) 
-        cou = 1;
-    else if (pass.value.includes(/A-Z/g))
-        cou = 2;
-    else if (pass.value.includes(/0-9/g))
-        cou = 3; 
 
-    return cou;
+
+function passStrength(password) { 
+    var st = document.getElementById('strength');
+    var stl = document.getElementById('strengthLabel'); 
+    var cou = 0; 
+    
+    if (password.match(/[a-z]+/)) cou += 1 
+    if (password.match(/[0-9A-Z]+/)) cou += 1  
+    if (password.match(/[~<>?]+/)) cou += 1 
+    if (password.match(/[!@#$%^&*()]+/)) cou += 1  
+    if (password.length>4) cou += 1  
+    if (password.length>7) cou += 1  
+
+
+    switch(cou) {
+        case 0:  st.value = 0; stl.innerHTML = "";  break
+        case 1:  st.value = 10; stl.innerHTML = "Weak";  break
+        case 2:  st.value = 25; stl.innerHTML = "Weak"; break
+        case 3:  st.value = 50; stl.innerHTML = "Good"; break
+        case 4:  st.value = 65; stl.innerHTML = "Good"; break
+        case 5:  st.value = 80; stl.innerHTML = "Strong"; break 
+        case 6:  st.value = 100; stl.innerHTML = "Very Strong"; break
+    } 
+    
 } 
